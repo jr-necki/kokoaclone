88313c087f0366305bd505329521fd1bd2b1f9cb
jelly-boop <jellyjelly111@naver.com>
2021-01-17 22:21:07 +0900

    노래넣음

---
+++ clock.js
@@ -0,0 +1,17 @@
+const clock=document.querySelector(".js-clock");
+
+function getTime(){
+
+  const date=new Date();
+  const minutes=date.getMinutes();
+  const hours=date.getHours();
+  const seconds=date.getSeconds();
+
+  clock.innerText=`${hours <10 ? `0${hours}`: hours}: ${minutes <10 ? `0${minutes}`:minutes}:${seconds<10 ? `0${seconds}`: seconds}`
+}
+function init(){
+    getTime();
+  setInterval(getTime, 1000);
+
+}
+init();
